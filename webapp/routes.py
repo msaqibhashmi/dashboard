6163cc6c5e250596f13874ef295bc26b127b0e54
Saqib Hashmi <saqib.hashmi@gaditek.com>
2018-11-27 07:42:04 +0000

    Form validation improved

--- webapp/routes.py
+++ webapp/routes.py
@@ -9,7 +9,7 @@ from flask import render_template, url_for, request, session, redirect, flash
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
         user = User(username=form.username.data, email=form.email.data)
